bff35334467a74093ccb3c87dec530e090de34cb
saikatnoman12 <noommaa240@gmail.com>
2022-12-25 10:16:26 +0600

    complete header design

---
+++ src/app/Shared/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { TruthyFalsyService } from './../../services/truthy-falsy.service';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  providers:[
+    TruthyFalsyService
+  ]
+})
+export class SharedModule { }
